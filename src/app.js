8d853d14586723929e6a214db831e5321dd7e22f
hiep ho <hiep.ho@zimaw.com>
2025-07-02 08:20:15 +0700

    insert data into database

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const cors = require('cors');
 const logger = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandle');
 const app = express();
+const path = require('path');
 
 const userRoutes = require('./routes/user.routes')
+
+// Serve ảnh trong public/uploads qua đường dẫn /uploads
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads/rooms')));
 
 app.use(cors());
 app.use(express.json());
