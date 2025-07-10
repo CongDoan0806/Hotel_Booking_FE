51edd9073a7522187e48d2eb1e9f011e92e7b6fc
hiep ho <hiep.ho@zimaw.com>
2025-07-10 16:07:49 +0700

    feat: done get amenities

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const authRoutes = require("./routes/auth.routes");
 const roomRoutes = require("./routes/room.routes");
 const bookingRoutes = require("./routes/booking.routes");
 const floorRoutes = require("./routes/floor.routes");
+const amenityRoutes = require("./routes/amenity.routes");
 // Serve ảnh trong public/uploads qua đường dẫn /uploads
 app.use(
   "/uploads/rooms",
@@ -26,4 +27,5 @@ app.use("/api/admin/rooms", adminRoutes);
 app.use("/api", authRoutes);
 app.use("/api", bookingRoutes);
 app.use("/api", floorRoutes);
+app.use("/api", amenityRoutes);
 module.exports = app;
