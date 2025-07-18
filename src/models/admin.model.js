0e5dc2a2af66abd5d31e333c684e47a49801b473
@vinh.tran <vinh.tran@student.passerellesnumeriques.org>
2025-07-18 08:48:15 +0700

    fix: change logic status filter

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -1,23 +1,22 @@
-const pool = require('../config/db');
+const pool = require("../config/db");
 
 const getUserListModel = async () => {
-    const query = `
+  const query = `
         SELECT 
             u.*, 
             b.booking_id, b.status AS booking_status, b.payment_status, 
             bd.booking_detail_id, bd.service_name, bd.quantity, bd.price_per_unit, bd.check_in_date, bd.check_out_date,
             r.room_id, r.name AS room_name, r.description AS room_description, r.price AS room_price, r.room_type_id, r.floor_id
         FROM users u
-        LEFT JOIN bookings b ON u.user_id = b.user_id
+        INNER JOIN bookings b ON u.user_id = b.user_id
         LEFT JOIN booking_details bd ON b.booking_id = bd.booking_id
         LEFT JOIN rooms r ON bd.room_id = r.room_id
         ORDER BY u.user_id ASC, b.booking_id ASC, bd.booking_detail_id ASC;
     `;
 
-    const result = await pool.query(query);
-    return result.rows;
-}
-
+  const result = await pool.query(query);
+  return result.rows;
+};
 const getCheckinGuestsModel = async () => {
   const query = `
     SELECT 
@@ -81,13 +80,13 @@ const getAdminDashboardStatusModel = async () => {
        (SELECT COUNT(*) 
        FROM rooms 
        WHERE status = 'booked') AS booked_room_count;
-    `
+    `;
   const result = await pool.query(query);
   return result.rows[0];
-  }
+};
 
-  const getAdminDashboardDealModel = async () => {
-    const query = `
+const getAdminDashboardDealModel = async () => {
+  const query = `
     SELECT 
         rt.name AS room_type_name,
         rt.default_price,
@@ -100,14 +99,14 @@ const getAdminDashboardStatusModel = async () => {
       GROUP BY rt.room_type_id, rt.name, rt.default_price
       ORDER BY rt.name;
       `;
-    const result = await pool.query(query);
-    return result.rows; 
-  }
+  const result = await pool.query(query);
+  return result.rows;
+};
 
 module.exports = {
-    getUserListModel,
-    getCheckinGuestsModel,
-    getCheckoutGuestsModel,
-    getAdminDashboardStatusModel,
-    getAdminDashboardDealModel
+  getUserListModel,
+  getCheckinGuestsModel,
+  getCheckoutGuestsModel,
+  getAdminDashboardStatusModel,
+  getAdminDashboardDealModel,
 };
