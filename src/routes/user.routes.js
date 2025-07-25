e733cbf35366b4b14874ec59c3f51692f9bf78e7
hiep ho <hiep.ho@zimaw.com>
2025-07-25 14:59:03 +0700

    feate: update avatar

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const userController = require("../controllers/user.controller");
 const authenticate = require('../middlewares/auth');
-
+const uploadAvatar = require("../middlewares/uploadAvatar");
 const router = express.Router();
 
 router.get("/users/:id", userController.getUser);
 
-router.put("/users/:id", userController.updateUser);
+router.put("/users/:id", uploadAvatar.single("avatar"), userController.updateUser);
 
 module.exports = router;
