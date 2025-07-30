9f19ba5c54ef44066756ee2f8a3b4b107f3cbd0f
binhnexusx <binhhoangdev9@gmail.com>
2025-07-30 10:06:34 +0700

    feat: implement backend APIs for room search and filter functionality

--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const roomController = require("../controllers/room.controller");
 router.get("/rooms", roomController.getAllRooms);
-router.get('/rooms/filter', roomController.filterRooms);
+router.get("/rooms/filter", roomController.filterRooms);
 router.get("/rooms/:id/details", roomController.getRoomDetail);
+router.get("/rooms/filter-options", roomController.getFilterOptions);
 
 module.exports = router;
