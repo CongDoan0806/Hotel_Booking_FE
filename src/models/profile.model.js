1fc902563d219b6e72182e121f9b6b206f1d9de0
MinhHoang1225 <minhhoang121225@gmail.com>
2025-08-04 11:29:34 +0700

    feat: display and delete the favorite rooms

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -10,6 +10,52 @@ const createHotelFeedbackModel = async (user_id, rating, comment) => {
   const result = await pool.query(query, [user_id, rating, comment]);
   return result.rows[0];
 };
+
+const getFavoriteRoomModel = async (user_id, limit, offset ) => {
+  const query = `
+    SELECT 
+      r.room_id,
+      r.name AS room_name,
+      rt.name AS room_type,
+      rl.name AS room_level,
+      r.description as room_description,
+      CONCAT('http://localhost:8000', MIN(ri.image_url)) AS image_url
+    FROM favorite_rooms fr
+    JOIN rooms r ON fr.room_id = r.room_id
+    JOIN room_types rt ON r.room_type_id = rt.room_type_id
+    JOIN room_levels rl ON r.room_level_id = rl.room_level_id
+    LEFT JOIN room_images ri ON r.room_id = ri.room_id
+    WHERE fr.user_id = $1
+    GROUP BY r.room_id, r.name, rt.name, rl.name
+    ORDER BY r.room_id
+    LIMIT $2 OFFSET $3
+   `;
+   const result = await pool.query(query, [user_id, limit, offset]);
+   return result.rows
+}
+
+const countFavoriteRoomModel = async (user_id) => {
+  const query =  `
+    SELECT COUNT(*) AS total
+    FROM favorite_rooms
+    WHERE user_id = $1
+  `;
+  const result = await pool.query(query, [user_id]);
+  return result.rows[0].total;
+}
+
+const deleteFavoriteRoomModel = async (user_id, room_id) => {
+  const query = `
+    DELETE FROM favorite_rooms
+    WHERE user_id = $1 AND room_id = $2
+  `;
+  const result = await pool.query(query, [user_id, room_id]);
+  return result.rowCount > 0; 
+};
+
 module.exports = {
-    createHotelFeedbackModel
+    createHotelFeedbackModel,
+    getFavoriteRoomModel,
+    countFavoriteRoomModel,
+    deleteFavoriteRoomModel
 }
