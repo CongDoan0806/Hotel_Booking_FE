bd4edb4c746df2dd73bb2e956ab4d2a5864e904e
binhnexusx <binhhoangdev9@gmail.com>
2025-08-06 14:54:01 +0700

    fix fea front desk

--- src/routes/booking.routes.js
+++ src/routes/booking.routes.js
@@ -14,6 +14,7 @@ router.get(
   authenticate,
   bookingController.getBookingDetailsByUserIdController
 );
+router.get("/bookings/all", bookingController.getAllBookingDetailsController);
 router.get(
   "/bookings/:booking_id",
   authenticate,
@@ -30,7 +31,6 @@ router.get(
   getBookingSummaryDetailController
 );
 router.post("/booking/auto-status", bookingController.handleAutoUpdateStatus);
-router.get("/reservations", bookingController.getAllBookingDetailsController);
 router.get(
   "/rooms/:roomId/disabled-dates",
   bookingController.getDisabledDatesController
