004fa77bc7a069a52a5ccbb64fc40f783fa4921a
Tran Cong Doan <congdoan0806@gmail.com>
2025-09-21 20:12:05 +0700

    Deploy code

--- src/config/db.js
+++ src/config/db.js
@@ -1,25 +1,3 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
-
-// pool.connect((err, client, release) => {
-//   if (err) {
-//     console.error('PostgreSQL connection error:', err.stack);
-//   } else {
-//     console.log('PostgreSQL connection successful');
-//     release(); 
-//   }
-// });
-
-// module.exports = pool;
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
