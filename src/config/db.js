8ea7de8af7edbf6cc368f14336ae6cb736aae76b
Tran Cong Doan <congdoan0806@gmail.com>
2025-09-21 20:10:59 +0700

    Deploy code

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,34 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     console.error('PostgreSQL connection error:', err.stack);
+//   } else {
+//     console.log('PostgreSQL connection successful');
+//     release(); 
+//   }
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false, // quan trọng để tránh lỗi SSL khi deploy
+  },
 });
 
 pool.connect((err, client, release) => {
@@ -14,7 +36,7 @@ pool.connect((err, client, release) => {
     console.error('PostgreSQL connection error:', err.stack);
   } else {
     console.log('PostgreSQL connection successful');
-    release(); 
+    release();
   }
 });
 
